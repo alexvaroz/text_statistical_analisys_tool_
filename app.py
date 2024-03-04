b708c72e03b51db746527242c835e402a66a1753
Alexandre Roriz <alexandre.vaz@gmail.com>
2024-03-04 02:59:06 +0000

    first version making the count and wordcloud

--- app.py
+++ app.py
@@ -3,13 +3,29 @@ import re
 from collections import Counter
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+import nltk
+nltk.download('stopwords')
+
 
 def tokenizing_regex(text):
   regex = r'\w+'
   return re.findall(regex,text.lower())
 
+
+def stopwords_removal(tokens, language='english'):
+   stopwords = nltk.corpus.stopwords.words(language)
+   tokens_limpos=[]
+   for item in tokens:
+    if (item not in stopwords) & (len(item) > 2) :
+       tokens_limpos.append(item)
+   return tokens_limpos
+
+
 def counting_tokens(tokens, top_frequencies=15):
     return Counter(tokens).most_common(top_frequencies)  
+
 
 def ploting_most_frequent_woords_bar(words_tuple):
     number_words = len(words_tuple)
@@ -23,13 +39,34 @@ def ploting_most_frequent_woords_bar(words_tuple):
     fig.update_xaxes(tickangle = -45)
     return fig
 
+def word_cloud_generator(tokens):
+   str_tokens = " ".join(s for s in tokens)
+   wordcloud = wordcloud = WordCloud(background_color="#f5f5f5").generate(str_tokens)
+   return wordcloud
+
+
 
 st.title("Text's Statistical Analyzer Tool")
 
-text_input = st.text_area("Text input")
+text_input = st.text_area("Text to analyse")
+
 
 
 if text_input != '':
-   word_tuples = counting_tokens(tokenizing_regex(text_input))
-   st.plotly_chart(ploting_most_frequent_woords_bar(word_tuples), use_container_width=True)
-   st.text(word_tuples)
+   run_buton = st.button("Run")
+   if run_buton:
+      tokens = tokenizing_regex(text_input)
+      tokens = stopwords_removal(tokens, "portuguese")
+      word_tuples = counting_tokens(tokens)
+      st.plotly_chart(ploting_most_frequent_woords_bar(word_tuples), use_container_width=True)
+      fig = plt.figure(figsize=(10, 12))
+      plt.imshow(word_cloud_generator(tokens), interpolation="bilinear")
+      plt.axis("off")
+      plt.tight_layout(pad=0)
+      st.pyplot(fig)
+      
+      reset_buton = st.button("Reset")
+      if reset_buton:
+        text_input = ''
+
+
